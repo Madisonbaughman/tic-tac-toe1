96a50e504e16fe2b2071e74d5fcb20c172be607e
Madisonbaughman <89171880+Madisonbaughman@users.noreply.github.com>
2023-07-16 21:13:47 -0400

    basic UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './App.css';
+
+function Board(){
+  const boardArr = new Array(9).fill('')
+
+  let item = boardArr.map((n, index) => 
+    <div key={index} className='space col-4'>{n}</div>
+    )
+  
+  return(
+    <div className='container'>
+      <div className='row'> 
+        {item}
+      </div>
+    </div>
+
+    // <div className='board container' >
+    //   <div className='first row'>
+    //     <div className='space col'>kjjkl</div>
+    //     <div className='space col' >{boardArr[1]}</div>
+    //     <div className='space col' >{boardArr[2]}</div>
+    //   </div>
+    // </div>
+
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Tic Tac Toe</h1>
+      <Board />
     </div>
   );
 }
