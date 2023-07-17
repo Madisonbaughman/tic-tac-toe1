2768baf62cdcad427e2ff9b83ce04dc552e57104
Madisonbaughman <89171880+Madisonbaughman@users.noreply.github.com>
2023-07-16 22:35:02 -0400

    beginning Functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function Board(){
-  const boardArr = new Array(9).fill('')
+  let [boardArr, setBoardArr] = useState(() => new Array(9).fill({value: ' '}))
+  let [toggle, setToggle] = useState('X');
 
-  let item = boardArr.map((n, index) => 
-    <div key={index} className='space col-4'>{n}</div>
+  let displayBoard = boardArr.map((n, index) => 
+    <div key={index} id={index} className='space col-4'>{n.value}</div>
     )
+  // could I have attached onClick here to pass in index ^^ ? but then i have 9 click handlers
   
+  function XorO(e){
+    
+    if (e.target.className !== 'row' && e.target.innerHTML === ' ' ){
+      console.log(e.target.id)
+      // the value of item in array equals the toggle
+      let copyBoard = [...boardArr]
+      copyBoard[e.target.id] = {value: toggle}
+      setBoardArr(copyBoard)
+      // reset the board
+      toggle === "X" ? setToggle('O') : setToggle('X') 
+      //run a fucntion to see if anyone has won
+      
+    }
+  }
+
   return(
     <div className='container'>
-      <div className='row'> 
-        {item}
+      <div className='row' onClick={XorO}> 
+        {displayBoard}
       </div>
     </div>
-
-    // <div className='board container' >
-    //   <div className='first row'>
-    //     <div className='space col'>kjjkl</div>
-    //     <div className='space col' >{boardArr[1]}</div>
-    //     <div className='space col' >{boardArr[2]}</div>
-    //   </div>
-    // </div>
 
   )
 }
